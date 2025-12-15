1b143af46b6ca59135e67d0f0c600f0a4282223a
Dr. Lindsay Jordan Browne <lindsay.jordan442@gmail.com>
2025-12-15 12:58:01 -0500

    Create Consent.js

---
+++ Consent.js
@@ -0,0 +1,69 @@
+define(['questAPI'], function(Quest){
+    let API = new Quest();
+    let isTouch = API.getGlobal().$isTouch;
+	
+    /**
+	* Page prototype
+	*/
+    API.addPagesSet('basicPage',{
+        noSubmit:false, //Change to true if you don't want to show the submit button.
+        header: 'Questionnaire',
+        decline: true,
+        declineText: isTouch ? 'Decline' : 'Decline to Answer', 
+        autoFocus:true, 
+        progressBar:  'Page <%= pagesMeta.number %> out of 3'
+    });
+	
+    /**
+	* Question prototypes
+	*/
+    API.addQuestionsSet('basicQ',{
+        decline: 'true',
+        required : true, 		
+        errorMsg: {
+            required: isTouch 
+                ? 'Please select an answer'' 
+                : 'Please select an answer''
+        },
+        autoSubmit:'true',
+        numericValues:'true',
+        help: '<%= pagesMeta.number < 3 %>',
+        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
+    });
+
+    API.addQuestionsSet('basicSelect',{
+        inherit :'basicQ',
+        type: 'selectOne'
+    });
+	
+	
+    /**
+	*Specific questions
+	*/	
+    API.addQuestionsSet('cYN',{
+        inherit : 'basicSelect',
+        name: 'attributes7',
+        stem: 'Do you agree to the research described above',
+        answers: [
+            {text:'Yes.',value:1},
+            {text:'No.',value:0},
+        ]
+    });
+    API.addSequence([
+        {
+            mixer : 'random', 
+            data : [
+                {
+                    mixer : 'random', 
+                    wrapper:true, 
+                    data : [
+                {
+                    inherit:'basicPage', 
+                    questions: {inherit:'attributes7'}
+                }
+            ]
+        }
+    ]);
+
+    return API.script;
+});
