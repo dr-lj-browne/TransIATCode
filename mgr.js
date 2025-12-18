3ca6895a88f47fd1308e2d71e1f58f8b84d117e2
Dr. Lindsay Jordan Browne <lindsay.jordan442@gmail.com>
2025-12-18 15:08:05 -0500

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -172,7 +172,7 @@ define(['managerAPI',
 		{
 		mixer:'branch',
 			conditions:[
-				{compare: 'response.ageyn', to: 'Yes.'}],
+				{compare: 'response.ageyn', to: 1}],
 			data:[
         {
             mixer:'random',
