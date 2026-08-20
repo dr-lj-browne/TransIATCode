53c3f210796840472fa2266d04b1ae2f4cf5862d
Dr. Lindsay Jordan Browne <lindsay.jordan442@gmail.com>
2026-08-20 19:38:36 -0400

    Update explicits.js
    testing something (original code)

--- explicits.js
+++ explicits.js
@@ -45,37 +45,51 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
         answers: [
-
-            {text:'4 - Extremely', value:4},
-            {text:'3 - Very', value:3},
-            {text:'2 - Moderately', value:2},
-            {text:'1 - Slightly', value:1},
-            {text:'0 - Not at all', value:0}
+            {text:'10 - Extremely warm', value:10},
+            {text:'9 - Very warm', value:9},
+            {text:'8 - Moderately warm', value:8},
+            {text:'7 - Somewhat warm', value:7},
+            {text:'6 - Slightly warm', value:6},
+            {text:'5 - Neither warm nor cold', value:5},
+            {text:'4 - Slightly cold', value:4},
+            {text:'3 - Somewhat cold', value:3},
+            {text:'2 - Moderately cold', value:2},
+            {text:'1 - Very cold', value:1},
+            {text:'0 - Extremely cold', value:0}
         ]
     });
-
 
 	
     /**
 	*Specific questions
 	*/	
-
-    API.addQuestionsSet('thermMasc',{
+    API.addQuestionsSet('attributes7',{
+        inherit : 'basicSelect',
+        name: 'attributes7',
+        stem: 'Which statement best describes you?',
+        answers: [
+            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
+            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
+            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
+            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
+            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
+            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
+            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
+        ]
+    });
+	
+    API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
-        name: 'thermMasc',
-        stem: 'How <b>masculine</b> would you say your gender identity is?'
+        name: 'Tblack_0to10',
+        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
     });
 
-    API.addQuestionsSet('thermFem',{
+    API.addQuestionsSet('thermWhite',{
         inherit : 'therm',
-        name: 'thermFem',
-        stem: 'How <b>feminine</b> would you say your gender identity is?'
+        name: 'Twhite_0to10',
+        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
     });
-    API.addQuestionsSet('thermAndro',{
-        inherit : 'therm',
-        name: 'thermAndro',
-        stem: 'How <b>androgynous or non-binary</b> would you say your gender identity is?'
-    });
+
     API.addSequence([
         {
             mixer : 'random', 
@@ -86,18 +100,19 @@ define(['questAPI'], function(Quest){
                     data : [
                         {
                             inherit:'basicPage', 
-                            questions: {inherit:'thermMasc'}
+                            questions: {inherit:'thermBlack'}
                         },
                         {
                             inherit:'basicPage', 
-                            questions: {inherit:'thermFem'}							
-                        },
-						{
-                            inherit:'basicPage', 
-                            questions: {inherit:'thermAndro'}							
+                            questions: {inherit:'thermWhite'}							
                         }
                     ]
+                },
+                {
+                    inherit:'basicPage', 
+                    questions: {inherit:'attributes7'}
                 }
+            ]
         }
     ]);
 
