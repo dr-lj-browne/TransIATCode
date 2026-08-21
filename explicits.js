2c85cc7365dfe1446e7824688a2ca1d735a6154e
Dr. Lindsay Jordan Browne <lindsay.jordan442@gmail.com>
2026-08-20 20:09:49 -0400

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -45,17 +45,17 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
         answers: [
-            {text:'10', value:10},
-            {text:'9', value:9},
-            {text:'8', value:8},
-            {text:'7', value:7},
-            {text:'6', value:6},
-            {text:'5', value:5},
-            {text:'4', value:4},
-            {text:'3', value:3},
-            {text:'2', value:2},
-            {text:'1', value:1},
-            {text:'0', value:0}
+            {text:'10 - Extremely warm', value:10},
+            {text:'9 - Very warm', value:9},
+            {text:'8 - Moderately warm', value:8},
+            {text:'7 - Somewhat warm', value:7},
+            {text:'6 - Slightly warm', value:6},
+            {text:'5 - Neither warm nor cold', value:5},
+            {text:'4 - Slightly cold', value:4},
+            {text:'3 - Somewhat cold', value:3},
+            {text:'2 - Moderately cold', value:2},
+            {text:'1 - Very cold', value:1},
+            {text:'0 - Extremely cold', value:0}
         ]
     });
 
@@ -63,42 +63,61 @@ define(['questAPI'], function(Quest){
     /**
 	*Specific questions
 	*/	
-
-    API.addQuestionsSet('Masc',{
+    API.addQuestionsSet('attributes7',{
+        inherit : 'basicSelect',
+        name: 'attributes7',
+        stem: 'Which statement best describes you?',
+        answers: [
+            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
+            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
+            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
+            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
+            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
+            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
+            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
+        ]
+    });
+	
+    API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
-        name: 'Mascul',
-        stem: 'On a scale of 1-10, how masculine would you say your gender identity is? </b>?'
+        name: 'Tblack_0to10',
+        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
     });
 
-    API.addQuestionsSet('Fem',{
+    API.addQuestionsSet('thermWhite',{
         inherit : 'therm',
-        name: 'Femin',
-        stem: 'On a scale of 1-10, how feminine would you say your gender identity is?</b>?'
-    });
-	API.addQuestionsSet('And',{
-        inherit : 'therm',
-        name: 'Andro',
-        stem: 'On a scale of 1-10, how androgynous would you say your gender identity is?</b>?'
+        name: 'Twhite_0to10',
+        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
     });
 
     API.addSequence([
+        {
+            mixer : 'random', 
+            data : [
                 {
                     mixer : 'random', 
                     wrapper:true, 
                     data : [
                         {
                             inherit:'basicPage', 
-                            questions: {inherit:'Masc'}
+                            questions: {inherit:'thermBlack'}
                         },
                         {
                             inherit:'basicPage', 
-                            questions: {inherit:'Fem'}							
+                            questions: {inherit:'thermWhite'}							
                         }
 						{
                             inherit:'basicPage', 
-                            questions: {inherit:'And'}							
+                            questions: {inherit:'thermWhite'}							
                         }
+                    ]
+                },
+                {
+                    inherit:'basicPage', 
+                    questions: {inherit:'attributes7'}
+                }
             ]
+        }
     ]);
 
     return API.script;
