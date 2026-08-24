a130a7c7aac35771bf4d70a657dc3225158cb8a2
Dr. Lindsay Jordan Browne <lindsay.jordan442@gmail.com>
2026-08-24 18:39:01 -0400

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -115,7 +115,7 @@ define(['managerAPI',
         redirect:
         [{ 
 			//Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-            type:'redirect', name:'redirecting', url: 'https://www.google.com/search' 
+            type:'redirect', name:'redirecting', url: `https://qualtricsxmkl3hyj2vp.qualtrics.com/jfe/form/SV_7PLTkuDY3QqjGf4?sessionID=${sessionID}`
         }],
 		
 		//This task waits until the data are sent to the server.
